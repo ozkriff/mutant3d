96742bacc9ebf00e2c805cf8e3d1335bbb61a1d4
ozkriff <ozkriff@gmail.com>
2012-04-16 14:18:21 +0400

    Moved set_xyz(), set_xy(), set_rgb() to gl.

--- gl.h
+++ gl.h
@@ -10,3 +10,7 @@ typedef struct {
 bool load_texture(char *filename, GLuint *id);
 void win2world(int x, int y, V3f *p);
 void va_rotate(Va *va, Quat q);
+
+void set_xy(float *coords, int n, int i, int vi, float x, float y);
+void set_xyz(float *verts, int n, int i, int vi, float x, float y, float z);
+void set_rgb(GLubyte *colors, int n, int i, int vi, GLubyte r, GLubyte g, GLubyte b);
--- mutant3d.c
+++ mutant3d.c
@@ -220,41 +220,6 @@ void set_camera(void){
   glTranslatef(map_pos.x, map_pos.y, 0);
 }
 
-void set_xy(float *coords, int n, int i, int vi, float x, float y){
-  float *coord;
-  assert(n == 2 || n == 3 || n == 4);
-  assert(vi >= 0 && vi <= n);
-  assert(i >= 0);
-  coord = coords + (i * n * 2) + (2 * vi);
-  *(coord + 0) = x;
-  *(coord + 1) = y;
-}
-
-/*if(quads) n=4; if(tris) n=3*/
-/* i - polygon number, vi - vertex number*/
-void set_xyz(float *verts, int n, int i, int vi, float x, float y, float z){
-  float *vertex;
-  assert(n == 2 || n == 3 || n == 4);
-  assert(vi >= 0 && vi <= n);
-  assert(i >= 0);
-  vertex = verts + (i * n * 3) + (3 * vi);
-  *(vertex + 0) = x;
-  *(vertex + 1) = y;
-  *(vertex + 2) = z;
-}
-
-/* TODO rename arguments */
-void set_rgb(GLubyte *colors, int n, int i, int vi, GLubyte r, GLubyte g, GLubyte b){
-  GLubyte *color;
-  assert(n == 2 || n == 3 || n == 4);
-  assert(vi >= 0 && vi <= n);
-  assert(i >= 0);
-  color = colors + (i * n * 3) + (3 * vi);
-  color[0] = r;
-  color[1] = g;
-  color[2] = b;
-}
-
 void draw_active_block(V3i p){
   GLfloat va[28 * 3] = {0};
   float n = BLOCK_SIZE / 2.0f;
