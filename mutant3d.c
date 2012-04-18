e584a033447e4f3ef759ce6291fe2f10a8d74bf3
ozkriff <ozkriff@gmail.com>
2012-04-18 16:46:11 +0400

    mutant3d.c: v3i_to_v3f(): Check block.

--- mutant3d.c
+++ mutant3d.c
@@ -104,7 +104,8 @@ V3f v3i_to_v3f(V3i i){
   f.x = (float)i.x * BLOCK_SIZE;
   f.y = (float)i.y * BLOCK_SIZE;
   f.z = (float)i.z * BLOCK_SIZE_2;
-  f.z += (BLOCK_SIZE_2 / BLOCK_HEIGHT) * (float)b->h;
+  if(b)
+    f.z += (BLOCK_SIZE_2 / BLOCK_HEIGHT) * (float)b->h;
   return f;
 }
 
