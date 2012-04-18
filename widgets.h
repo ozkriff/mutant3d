b20abd9ff63741208c423eb62fb7d517c139b8a6
ozkriff <ozkriff@gmail.com>
2012-04-18 21:28:10 +0400

    widgets.h: Added prototype for change_button_by_id().

--- widgets.h
+++ widgets.h
@@ -17,3 +17,4 @@ Button *v2i_to_button(V2i pos);
 void add_button(TTF_Font *f, int id, V2i pos, char *text, void (*callback)(void));
 void draw_buttons(void);
 void change_button_text(Button* b, char* text);
+void change_button_text_by_id(int id, char* text);
